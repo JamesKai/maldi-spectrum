a5284e3d397ac4e69ccff8161111d9e67d08be4f
James <kai.hsiang.ju@gmail.com>
2020-10-31 23:40:09 +0800

    add label to peak

--- spectrum_test.py
+++ spectrum_test.py
@@ -22,20 +22,32 @@ def read_file(file_path: str) -> Tuple[List[float], List[float]]:
 
 
 def generate_graph(file_path: str):
+    annotate_mz = []
     file_name = os.path.basename(file_path[:-4])
-    # matrix = file_name.split("_")[0]
-    # conc = file_name.split("_")[1]
-    print("kai3")
 
+    print("kai3")
     mz, intensity = read_file(f"{file_path}")
     spectrum = sus.MsmsSpectrum(file_name, 1.0, 1, mz, intensity,)
+    annotate_mz = set(
+        [
+            int(m)
+            for i, m in zip(spectrum.intensity, spectrum.mz)
+            if i / spectrum.intensity.max() >= 0.10
+        ]
+    )
+    smplfy_new_lst = []
+    for i in annotate_mz:
+        if smplfy_new_lst == [] or abs(smplfy_new_lst[-1] - i) > 3:
+            smplfy_new_lst.append(i)
+
+    print(smplfy_new_lst)
+    for ano_mz in smplfy_new_lst:
+        spectrum.annotate_mz_fragment(
+            ano_mz, 1, 3, "Da",
+        )
     print("kai4")
-    # p2s_mz = 1116
-    # spectrum.annotate_mz_fragment(p2s_mz, 1, 3, "Da", text=f"{p2s_mz}\nTriP-2S")
-    # # spectrum.annotate_mz_fragment(902, 1, 3, "Da")
-    # spectrum.annotate_mz_fragment(656, 1, 3, "Da")
+
     fig, ax = plt.subplots(figsize=(12, 6))
-    # ax.set_title(f"${conc}$ Tri-P2S in {matrix} Matrix")
     save_path = os.path.join(os.path.dirname(file_path), "graph")
     try:
         os.makedirs(save_path)
@@ -60,7 +72,7 @@ def process_files(file_dir: str):
 
 
 def main():
-    process_files(file_dir="test")
+    process_files(file_dir="20201030")
 
 
 if __name__ == "__main__":
